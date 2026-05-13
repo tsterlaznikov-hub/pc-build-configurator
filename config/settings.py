82080091369cea474e676ff5eb925ea8d584b811
Тимофей Стерляжников <tsterlaznikov@gmail.com>
2026-05-13 15:48:43 +0500

    Add PythonAnywhere to ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'django-insecure-dev-key-change-in-production'
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'tsterlyazhnikov.pythonanywhere.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
